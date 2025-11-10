66b52cf9fec27aacc75a4afd7a65ff3a9d367440
mokshithakummara78-droid <mokshithakummara78@gmail.com>
2025-11-10 14:46:48 +0530

    Add files via upload

---
+++ arthematic1.c
@@ -0,0 +1,14 @@
+//C program to excute Arthematic operators
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter two values:");
+	scanf("%d,%d",&a,&b);
+	printf("sum of two numbers=%d\n",a+b);
+	printf("subtraction of two numbers=%d\n",a-b);
+	printf("multiplication of two numbers=%d\n",a*b);
+	printf("division of two numbers=%d\n",a/b);
+	printf("moduluo division of two numbers=%d\n",a%b);
+	return 0;
+}
