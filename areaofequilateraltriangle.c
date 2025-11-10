1cc4fd250aeac8dc43d80901fad88c99c50be246
mokshithakummara78-droid <mokshithakummara78@gmail.com>
2025-11-10 14:54:09 +0530

    Add files via upload

---
+++ areaofequilateraltriangle.c
@@ -0,0 +1,11 @@
+//C program to calculate area of equilateral triangle
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int s=5;
+	float area;
+	area=sqrt(3)/4*s*s;
+	printf("Area of equilateral triangle=%f",area);
+	return 0;
+}
