ca48637587233eb5147800a0e67683a9efe165d7
mokshithakummara78-droid <mokshithakummara78@gmail.com>
2025-11-10 14:54:43 +0530

    Add files via upload

---
+++ areaofrectangle.c
@@ -0,0 +1,9 @@
+//C program to calculate area of rectangle
+#include <stdio.h>
+int main( )
+{
+	int length=25,breadth=4,area;
+	area=length*breadth;
+	printf("area of rectangle=%d",area);
+	return 0;
+}
