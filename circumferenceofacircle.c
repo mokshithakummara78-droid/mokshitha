0febd4d6183be44d3785f2466cde64da11bbc262
mokshithakummara78-droid <mokshithakummara78@gmail.com>
2025-11-12 14:19:05 +0530

    Add files via upload

---
+++ circumferenceofacircle.c
@@ -0,0 +1,9 @@
+//C program to calculate circumference of a circle
+# include<stdio.h>
+int main( )
+{
+	float PI=3.14,r=1.2,circumference;
+	circumference=2*PI*r;
+	printf("circumference of a circle=%f",circumference);
+	return 0;
+}
