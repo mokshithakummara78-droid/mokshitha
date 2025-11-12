0d557a60199abc0ee8d65693f4d4a018d38e9eb9
mokshithakummara78-droid <mokshithakummara78@gmail.com>
2025-11-12 14:26:16 +0530

    Add files via upload

---
+++ division.c
@@ -0,0 +1,10 @@
+//C program to calculate division of two numbers
+#include <stdio.h>
+int main( )
+{
+	int x=25,y=6;
+	float div;
+	div=(float)x/y;
+	printf("division of two numbers=%f",div);
+	return 0;
+}
